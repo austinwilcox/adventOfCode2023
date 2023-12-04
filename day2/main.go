de61a8f84fb531bf3b2412ee78a0ffa455e00a3a
Austin Wilcox <awilcox@arbinger.com>
2023-12-03 21:22:20 -0700

    add solution for day 2 part one
    The solution was simple, just a lot of string splitting.

--- day2/main.go
+++ day2/main.go
@@ -1,11 +1,56 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
+//NOTE: Which games would be possible in the list if there are only:
+//12 red cubes
+//13 green cubes
+//14 blue cubes
+func main() {
+  example, err := os.ReadFile("input.txt"); if err != nil {
+    log.Fatal(err)
+  }
+  splitArr := strings.Split(string(example), "\n")
+  sum := 0
+  for _, line := range splitArr {
+    if line == "" {
+      continue
+    }
+    sum += PartOne(line)
+  }
+
+  fmt.Println(sum)
 }
 
-func PartOne() {
+func PartOne(line string) int {
+  dp := make(map[string]int, 3)
+  dp["red"] = 12
+  dp["green"] = 13
+  dp["blue"] = 14
 
+  var regex = regexp.MustCompile(`^Game (\d+)`)
+  gameId, _ := strconv.Atoi(regex.FindStringSubmatch(line)[1])
+
+  splitArr := strings.Split(strings.Split(line, ":")[1], ";")
+  for _, game := range splitArr {
+    for _, cube := range strings.Split(game, ",") {
+      cube = strings.Trim(cube, " ")
+      values := strings.Split(cube, " ")
+      intVal, _ := strconv.Atoi(values[0])
+      if dp[values[1]] < intVal {
+        return 0
+      }
+    }
+  }
+
+  return gameId
 }
 
 func PartTwo() {
